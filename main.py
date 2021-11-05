9db8dcbaa7f25182bfa3daeba1e5b9279c7de3a0
JVN313 <82493100+JVN313@users.noreply.github.com>
2021-11-05 04:59:36 -0400

    added image getter

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from bs4 import BeautifulSoup
 import requests
 pokemon = input("Enter Pokemon Name: ")
-site = requests.get(f"https://pokemondb.net/pokedex/{pokemon}").text
-soup = BeautifulSoup(site, "lxml")
-stats = soup.find("div", class_="resp-scroll").text
+def poke_stat_getter(pokemon):
+    site = requests.get(f"https://pokemondb.net/pokedex/{pokemon}").text
+    soup = BeautifulSoup(site, "lxml")
+    stats = soup.find("div", class_="resp-scroll").text
+    #stat_list = stats.split()
+    print(stats)
 
-print(stats)
+def poke_image_getter(pokemon):
+     r = requests.get(f"https://img.pokemondb.net/artwork/{pokemon}.jpg")
+     image_open= open(f"{pokemon}.jpg", "wb")
+     image_open.write(r.content)
+     image_open.close()
+
+poke_stat_getter(pokemon)
+poke_image_getter(pokemon)
