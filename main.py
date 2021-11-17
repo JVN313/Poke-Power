7fbc8e32cad69c44aee4a0af7ed637fc8bab8b65
JVN313 <82493100+JVN313@users.noreply.github.com>
2021-11-17 11:02:02 -0500

    added title to pdf and evolution info

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from ctypes import alignment
 from bs4 import BeautifulSoup
 from fpdf import FPDF
 import requests
 import os
 
 pokemon = input("Enter Pokemon Name: ")
-
+    #TODO format varible strings for pdf text
 def poke_stat_getter(pokemon):
+    global evolution, poke_name
     site = requests.get(f"https://pokemondb.net/pokedex/{pokemon}").text
     soup = BeautifulSoup(site, "lxml")
     stats = soup.find("div", class_="resp-scroll").text
+    evolution = soup.find("div", class_="infocard-list-evo").text
+    poke_name = soup.find("h1").text
+    full_stats = str(stats) + str(evolution)
     return str(stats)
 
 def poke_image_getter(pokemon):
@@ -17,12 +22,16 @@ def poke_image_getter(pokemon):
      image_open.write(r.content)
      image_open.close()
 
+    #TODO format pdf text
 def poke_pdf(pokemon):
     pdf = FPDF("P", "mm", "Letter")
     pdf.add_page()
-    pdf.image(f"{pokemon}.jpg")
+    pdf.set_font("helvetica", "", 55)
+    pdf.cell(0, 30, poke_name, ln=True, align="C")
+    pdf.image(f"{pokemon}.jpg", x=40 )
     pdf.set_font("helvetica", "", 16)
-    pdf.cell(40, 30, poke_stat_getter(pokemon))
+    pdf.cell(1000, 20, poke_stat_getter(pokemon), ln=True)
+    pdf.cell(0, 20, evolution, ln=True, align="C")
     pdf.output(f"Pokemon-Files/{pokemon}.pdf")
     os.remove(f"{pokemon}.jpg")
 
